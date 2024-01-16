6e7e8f3a0e5efd57a48b698796a44a6ca3d01632
FLASH230702 <tanishqjadhav2002@gmail.com>
2024-01-16 17:17:07 +0530

    final

--- main.js
+++ main.js
@@ -12,3 +12,14 @@ function calc(){
   let y = math.evaluate(n)
   document.getElementById("result").value = y;
 }
+
+function calnew(event) { 
+  if (event.key == '0' || event.key == '1' 
+      || event.key == '2' || event.key == '3' 
+      || event.key == '4' || event.key == '5' 
+      || event.key == '6' || event.key == '7' 
+      || event.key == '8' || event.key == '9' 
+      || event.key == '+' || event.key == '-' 
+      || event.key == '*' || event.key == '/') 
+      document.getElementById("result").value += event.key; 
+} 
